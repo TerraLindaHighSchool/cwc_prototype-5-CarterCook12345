6ccaed92775ee905047f15c05053e92f1284fd49
CarterCook12345 <89489627+CarterCook12345@users.noreply.github.com>
2022-05-19 16:29:42 -0700

    Updated Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,12 +9,14 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
-    public bool isGameActive;
     public Button restartButton;
     public GameObject titleScreen;
+
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
@@ -24,29 +26,30 @@ public class GameManager : MonoBehaviour
 
     public void StartGame(int difficulty)
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
         isGameActive = true;
-        titleScreen.gameObject.SetActive(false);
+        score = 0;
         spawnRate /= difficulty;
+        
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        
+        titleScreen.gameObject.SetActive(false);
     }
 
     public void GameOver()
     {
-        gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-            while (isGameActive);
         }
     }
 
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
+    private float minTorque = -10;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
-    private GameManager gameManager;
+    private float ySpawnPos = -2;
+
     public int pointValue;
     public ParticleSystem explosionParticle;
+    
+    private Rigidbody targetRb;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +26,8 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
         
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     private void OnMouseDown()
